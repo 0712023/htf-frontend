80bac6f69af5f4fa7dc422d19d45d107b3fc0403
0712023 <0712023@hanmail.net>
2021-02-04 16:42:38 +0900

    change sidebar's initial state not to show at home

--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,7 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    isNavOpen : false,
+    isNavOpen : true,
   },
   mutations: {
     [Constant.SETISNAVOPEN]: (oldState, payload) =>{
