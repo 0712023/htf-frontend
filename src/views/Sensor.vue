3c4f8e354f65b9b77d6ece4df275cc1d01beb004
0712023 <0712023@hanmail.net>
2021-02-05 16:04:31 +0900

    apply white letter style

--- src/views/Sensor.vue
+++ src/views/Sensor.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>
+    <div id="test">
         this sensor is : {{$route.params.desc}} <br>
     </div>
 </template>
+<style scoped>
+div#test { 
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 450px; height: 250px;
+    margin-left: -220px;
+    margin-top : -170px;
+    display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+    color: #fff;
+    text-decoration: none;
+    font-size: 1.5rem;
+}
+</style>
