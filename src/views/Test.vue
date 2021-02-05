11b74755026e0eb0d0fa8be07b720a139c5b0248
Jaeung Kim <jaeung949@gmail.com>
2021-02-05 09:18:21 +0900

    chart update

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
         this is test
+        <Chart/>
     </div>
 </template>
 <script>
 import Constant from '../store/Constant'
 import store from '../store/store'
+import Chart from '../components/Dashboard/Chart'
 export default {
+    components: {
+        Chart
+    },
     created:()=>{
         store.commit(Constant.TOGGLENAV)
     }
