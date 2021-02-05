4e5ce1f18a2e4b2c0bb6d3e1f957b4c206317839
Jaeung Kim <jaeung949@gmail.com>
2021-02-05 09:30:55 +0900

    directory update

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-        this is test
+    <div style="color='white'">
+        <h2 style="">차트 및 기타 정보로 대시보드화가 이루어지는 곳입니다</h2>
         <Chart/>
     </div>
 </template>
 <script>
 import Constant from '../store/Constant'
 import store from '../store/store'
-import Chart from '../components/Dashboard/Chart'
+import Chart from './Dashboard/Chart'
 export default {
     components: {
         Chart
