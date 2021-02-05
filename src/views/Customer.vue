b3e86056c8a8a9cd3327e7129ba113c253a743e9
0712023 <0712023@hanmail.net>
2021-02-05 21:00:14 +0900

    Update Cusdata.vue delete useless lines

--- src/views/Customer.vue
+++ src/views/Customer.vue
@@ -6,13 +6,9 @@
 
 <script>
 import Cusdata from '../components/Cusdata.vue';
-import { mutations } from "@/store/store.js";
 export default {
     components:{
         Cusdata
-    },
-     created:()=>{
-        mutations.toggleNav();
     }
 }
 </script>
