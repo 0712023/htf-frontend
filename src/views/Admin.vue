a1f3ff1dadb61c2a81a85c5a08f6035d1e93e7cb
0712023 <0712023@hanmail.net>
2021-02-06 10:52:13 +0900

    apply router link to user.vue

---
+++ src/views/Admin.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <div style="overflow:scroll; width:100%; height:100vh;">
+            <div  class="wrap">
+            <div v-for="(value, index) in user" :key="index">
+                <div class="box1" >
+                    유저 :<a :href="value.url">{{value.name }}</a> <br>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script type="module">
+export default {
+    data: function () {
+    return {
+      user: [
+        { name: "mg" ,url:"/" },
+        { name: "om" ,url:"/home"},
+        { name: "sh" ,url:"/home"},
+        { name: "jw" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+        { name: "ch" ,url:"/home"},
+      ],
+    };
+  },
+}
+</script>
--- src/views/User.vue
+++ src/views/User.vue
@@ -2,72 +2,19 @@
     <div>
         <div style="overflow:scroll; width:100%; height:100vh;">
             <div  class="wrap">
-            <div v-for="(value, index) in user" :key="index">
-                <div class="box1" >
-                    유저 :<a :href="value.url">{{value.name }}</a> <br>
+                <div class="box1" v-for="sensor in sensors" :key="sensor.seq" >
+                    <router-link :to="'/sensor/'+sensor.desc">{{ sensor.desc }}</router-link>
                 </div>
-            </div>
             </div>
         </div>
     </div>
 </template>
 
-<script type="module">
+<script>
+import store from "../store/store";
 export default {
-    data: function () {
-    return {
-      user: [
-        { name: "mg" ,url:"/" },
-        { name: "om" ,url:"/home"},
-        { name: "sh" ,url:"/home"},
-        { name: "jw" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-        { name: "ch" ,url:"/home"},
-      ],
-    };
+    data() {
+    return { sensors: store.state.routes };
   },
 }
 </script>
