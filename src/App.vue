e82213d630e008496550e7fd47892df12e542f66
0712023 <0712023@hanmail.net>
2021-02-06 12:41:28 +0900

    delete home at sidebar

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,6 @@
     <Sidebar>
       <ul class="sidebar-panel-nav">
         <li>
-          <router-link to="/">Home</router-link>
-        </li>
-        <li>
           <router-link to="/user">User</router-link>
         </li>
         <li>
