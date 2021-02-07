8d518ff682c158df09e5c0b003a84ff1fc69515d
0712023 <0712023@hanmail.net>
2021-02-07 15:00:27 +0900

    Update User.vue

--- src/views/User.vue
+++ src/views/User.vue
@@ -12,11 +12,12 @@
 </template>
 
 <script>
-import store from "../store/store";
 export default {
-    data() {
-        return { sensors: store.state.routes };
-    },
+    computed:{
+        sensors:function(){
+            return this.$store.state.routes;
+        }
+    }
     // 여기서 만약 토큰 로그인이 유효한 상태라면 액시오스를 다시 날려주는 로직이 있어야 새로고침을 해도 오류가 나지 않을 것으로 생각
 }
 </script>
