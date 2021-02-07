bc0b2c271fe2fc96c492dc588928b1d98141ca27
0712023 <0712023@hanmail.net>
2021-02-08 01:12:39 +0900

    visualize value of each sensor at user dashboard

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
         <div style="overflow:scroll; width:100%; height:100vh;">
-            <div  class="wrap">
-                <div class="box1" v-for="sensor in sensors" :key="sensor.mchId" >
+            <div  class="wrap" >
+                <div class="box1" v-for="sensor in sensors" :key="sensor.mchId">
                     <router-link :to="'/sensor/'+sensor.description + '/mchid/' + sensor.mchId">{{ sensor.description }}</router-link>
                     <br><br>name : {{ sensor.description }} 
                     <br><br>mchId : {{ sensor.mchId }}
+                    <br><br>value : {{ sensorDataStore[sensor.mchId] }}
                 </div>
             </div>
         </div>
@@ -13,13 +14,28 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
     computed:{
         sensors(){
             return JSON.parse(this.$cookie.get("sensors"));
-        }
-    }
-    // 여기서 만약 토큰 로그인이 유효한 상태라면 액시오스를 다시 날려주는 로직이 있어야 새로고침을 해도 오류가 나지 않을 것으로 생각
+        },
+    },
+    data(){
+        return {sensorDataStore:{}}
+    },
+    created:function(){
+        setInterval(()=>{
+                for(let index in this.sensors){
+                    axios.post(`http://studioj.ddns.net/getMeasureListByMchIdTo1`, {"mchId": this.sensors[index].mchId}, 
+                        {headers: { Authorization: `Bearer ${this.$cookie.get("accesstoken")}`}})
+                    .then(response =>{
+                        this.$set(this.sensorDataStore, this.sensors[index].mchId, response.data.value)
+                    })
+                }
+                console.log(this.sensorDataStore)
+            }, 2000);
+    },
 }
 </script>
 
