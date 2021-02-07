82a0e0c04ef62d809872c7bb06a99781f5478287
Jaeung Kim <jaeung949@gmail.com>
2021-02-07 17:49:11 +0900

    destroyed clearInterval

--- src/views/Dashboard/charts/BarChart.vue
+++ src/views/Dashboard/charts/BarChart.vue
@@ -23,32 +23,33 @@ import axios from 'axios';
     mounted () {
       this.fillData();
     },
-    created: () => {
-      axios
+    created() {
+      console.log(this.$route.params.mchId);
+
+      this.chartInterval = setInterval(()=>{
+      axios.post(`http://studioj.ddns.net/getMeasureListByMchId`, {"mchId": this.$route.params.mchId}, 
+        {headers: { Authorization: `Bearer ${this.$cookie.get("accesstoken")}`}}
+        ).then(response =>{
+          console.log(response.data);
+          this.datacollection.labels.push("");
+          for (let dataset of this.datacollection.datasets) {
+            dataset.data.push(parseInt(response.data[0].value) + Math.random()*5 );
+          }
+        })
+
+        if (this.datacollection.labels.length>9) {
+          this.datacollection.labels.shift();
+          for (let dataset of this.datacollection.datasets) {
+            dataset.data.shift();
+          }
+        }
+      }, 2000)
+    },
+    destroyed() {
+      clearInterval(this.chartInterval);
     },
     methods: {
       fillData () {
-        console.log(this.$route.params.mchId);
-
-        setInterval(()=>{
-        axios.post(`http://studioj.ddns.net/getMeasureListByMchId`, {"mchId": this.$route.params.mchId}, 
-          {headers: { Authorization: `Bearer ${this.$cookie.get("accesstoken")}`}}
-          ).then(response =>{
-            console.log(response.data);
-            this.datacollection.labels.push("");
-            for (let dataset of this.datacollection.datasets) {
-              dataset.data.push(parseInt(response.data[0].value) + Math.random()*5 );
-            }
-          })
-
-          if (this.datacollection.labels.length>9) {
-            this.datacollection.labels.shift();
-            for (let dataset of this.datacollection.datasets) {
-              dataset.data.shift();
-            }
-          }
-        }, 2000)
-
         this.datacollection = {
           labels: [this.getRandomInt(), this.getRandomInt(), this.getRandomInt(), this.getRandomInt()],
           datasets: [
