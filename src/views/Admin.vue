2bb44820470ba4766e1f25a7866189cc83195d67
Jaeung Kim <jaeung949@gmail.com>
2021-02-08 22:29:17 +0900

    Update Admin.vue

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -4,7 +4,7 @@
             <div  class="wrap">
             <div v-for="(value, index) in members" :key="index">
                 <div class="box1" >
-                    유저 : <a href="#" @click="toUser(value.memId)">{{value.memId}}</a>
+                    유저 : <a @click="toUser(value.memId)">{{value.memId}}</a>
                 </div>
             </div>
             </div>
@@ -13,16 +13,32 @@
 </template>
 
 <script type="module">
+import axios from 'axios';
+import EventBus from '../store/Eventbus';
 export default {
-    computed: {
-      members() {
-        return JSON.parse(this.$cookie.get("members"));
-      }
-    },
-    data: function () {
+  computed: {
+    members() {
+      return JSON.parse(this.$cookie.get("members"));
+    }
+  },
+  data: function () {
     return {
       user: [],
     };
   },
+  methods: {
+    toUser (v) {
+      console.log(v);
+      axios.post(`http://studioj.ddns.net/getMachineListByMemId`, {"memId": v}, 
+        {headers: { Authorization: `Bearer ${this.$cookie.get("accesstoken")}`}}
+      ).then(response =>{
+        console.log(response.data);
+        this.$cookie.set("sensors", JSON.stringify(response.data), 1);
+        console.log(this.$cookie.get("sensors"));
+        EventBus.$emit('sensors', response.data);
+        this.$router.push('../member/'+v);
+      })
+    }
+  }
 }
 </script>
