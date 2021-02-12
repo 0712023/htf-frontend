89e4d34b131f45bc1182c45a1d93901f530f0259
Jaeung Kim <jaeung949@gmail.com>
2021-02-12 21:53:03 +0900

    vendor login init

--- src/views/Login/VendorLogin.vue
+++ src/views/Login/VendorLogin.vue
@@ -7,8 +7,8 @@
 </template>
 
 <script>
-// import axios from 'axios';
-// import EventBus from '../../store/Eventbus';
+import axios from 'axios';
+import EventBus from '../../store/Eventbus';
 export default {
     data () {
         return {
@@ -19,33 +19,32 @@ export default {
     methods:{
         vendorLogin() {
             alert('test login');
-            // axios.post(`http://studioj.ddns.net/loginMember`, {"memId":this.id,"memPw":this.pw})
-            // .then(res => {
-            //     if (res.data == '') {
-            //         alert("login failed")
-            //         throw new Error("login failed")
-            //     } else {
-            //         //쿠키에 access token를 넣어줌
-            //         this.$cookie.set("accesstoken", res.data, 1);
-            //     }
+            axios.post(`http://studioj.ddns.net/loginVendor`, {"vendorId":this.id,"vendorPw":this.pw})
+            .then(res => {
+                if (res.data == '') {
+                    alert("login failed")
+                    throw new Error("login failed")
+                } else {
+                    //쿠키에 access token를 넣어줌
+                    this.$cookie.set("accesstoken", res.data, 1);
+                }
+                axios.defaults.headers.common["x-access-token"] = res.data;
+                axios.post(`http://studioj.ddns.net/getMachineListByVendorId`, {"vendorId": this.id},{headers: { Authorization: `Bearer ${this.$cookie.get("accesstoken")}`}})
+                .then(res =>{
+                    //로그인 정보 및 센서 데이터 쿠키에 저장
+                    this.$cookie.set("memId", this.id, 1);
+                    this.$cookie.set("login", "login", 1);
+                    this.$cookie.set("mchList", JSON.stringify(res.data), 1);
+                    //사이드바 및 로그아웃 버튼 활성화
+                    EventBus.$emit('login', true);
+                    EventBus.$emit('mchList', res.data);
 
-            //     //유저가 로그인을 시도하는 경우
-            //     axios.defaults.headers.common["x-access-token"] = res.data;
-            //     axios.post(`http://studioj.ddns.net/getMachineListByMemId`, {"memId": this.id},{headers: { Authorization: `Bearer ${this.$cookie.get("accesstoken")}`}})
-            //     .then(res =>{
-            //         //로그인 정보 및 센서 데이터 쿠키에 저장
-            //         this.$cookie.set("memId", this.id, 1);
-            //         this.$cookie.set("login", "login", 1);
-            //         this.$cookie.set("mchList", JSON.stringify(res.data), 1);
-            //         //사이드바 및 로그아웃 버튼 활성화
-            //         EventBus.$emit('login', true);
-            //         EventBus.$emit('member', true);
-            //         EventBus.$emit('mchList', res.data);
-            //         this.$router.push('member/'+this.id);
-            //     })
-            // }).catch(function(error){
-            //     console.log(error)
-            // })
+                    EventBus.$emit('member', true);
+                    this.$router.push('member/'+this.id);
+                })
+            }).catch(function(error){
+                console.log(error)
+            })
         },
     }
 }
