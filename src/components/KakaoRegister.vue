aaa42f2b4108315e04b94c31bf3c3cdcc5b83aea
0712023 <0712023@hanmail.net>
2021-02-14 00:46:20 +0900

    Update KakaoRegister.vue

--- src/components/KakaoRegister.vue
+++ src/components/KakaoRegister.vue
@@ -8,7 +8,7 @@
 export default {
     methods:{
         getKakaoToken(){
-            location.href='https://kauth.kakao.com/oauth/authorize?client_id='+this.RESTAPIKEY+'&redirect_uri='+this.REDIRECT_URI+'&response_type=code'
+            location.href='https://kauth.kakao.com/oauth/authorize?client_id='+this.RESTAPIKEY+'&redirect_uri='+this.REDIRECT_URI+'&response_type=code&scope=friends'
         },
     },
     data(){
