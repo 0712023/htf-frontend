79cb29a0827bcccdd2b423ae41cc2587d548fe0c
Jaeung Kim <jaeung949@gmail.com>
2021-02-16 12:37:39 +0900

    add machine type update in vendor

--- src/views/MachineManage.vue
+++ src/views/MachineManage.vue
@@ -1,39 +1,43 @@
 <template>
     <div id="standard">
         Machine List
-    <table border="1" width="1000">
+        <table border="1" width="1000">
             <thead> 
             <tr><!--template 쓰는 이유 : v-for 이후에 th에서 v-if사용하기 위해서 -->
                 <template v-for="(val, key) in mchList[0]">
                     <th width="500" :key="key" v-if="key!='memId'" >{{key}}</th>
                 </template>
+                <th>update type</th>
             </tr>
             </thead>
             <tbody>
                 <tr v-for="machine in mchList" :key="machine.mchId">
                     <template v-for="(val, key) in machine" >
-                        <td width="500" :key="key" v-if="key!='memId' && key!='vendorId'">{{val}}</td>
-                        <td width="500" :key="key" v-if="key=='vendorId'">{{val.vendorId}}</td>
+                        <td :key="key" v-if="key!='memId' && key!='vendorId'">{{val}}</td>
+                        <td :key="key" v-if="key=='vendorId'">{{val.vendorId}}</td>
                     </template>
+                    <td><button @click="updateMachineType(machine.type, machine.mchId)">update</button></td>
                 </tr>
             </tbody>
         </table> 
         <br>
         <button @click="modalshow">Add</button>
         <modal name="MachineRegister"><MachineRegister/></modal>
+        <modal name="UpdateMachineType"><UpdateMachineType :type.sync="type" :mchIdProps.sync="mchId"/></modal>
     </div>
 </template>
 
 <script>
 import axios from 'axios'
 import MachineRegister from './Modal/MachineRegister'
+import UpdateMachineType from './Modal/UpdateMachineType'
 import EventBus from '../store/Eventbus'
 export default {
     data(){
-        return {mchList:[]}
+        return {mchList:[], type:'',mchId:''}
     },
     components:{
-        MachineRegister
+        MachineRegister,UpdateMachineType
     },
     mounted() {
         this.$modal.hide('MachineRegister');
@@ -54,7 +58,12 @@ export default {
             .catch((err)=>{
                 console.log(err);
             })
-        }
+        },
+        updateMachineType(type,mchIdInPut) {
+            this.mchId = mchIdInPut;
+            this.type = type;
+            this.$modal.show("UpdateMachineType", {"type":type});
+        },
     }
 }
 </script>
---
+++ src/views/Modal/UpdateMachineType.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="standard2">
+        Machine Id : {{mchIdProps}}
+        <br><br>
+        New Type : <span><input type="text" v-model="newType"></span>
+        <button @click="updateMachineType">수정 완료</button>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import EventBus from "../../store/Eventbus";
+export default {
+    data(){
+        return {
+            newType: this.type,
+        }
+    },
+    props:{type : String, mchIdProps : String},
+    methods:{
+        updateMachineType() {
+            axios.post(`${this.$store.state.BACK_SERVER}/updateMachineType`,{ mchId: this.mchIdProps , type: this.newType},{headers: {Authorization: `Bearer ${this.$cookie.get("accesstoken")}`}})
+            .then(() => {
+            alert("type update success!");
+            EventBus.$emit("modal", false);
+            })
+            .catch(function (error) {
+            console.log(error);
+            });
+        },
+    }
+}
+</script>
