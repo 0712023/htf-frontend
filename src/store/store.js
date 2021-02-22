2a2533bd087889e4b8a3e430d268d0b6437843b7
0712023 <0712023@hanmail.net>
2021-02-22 13:52:51 +0900

    change kako app

--- src/store/store.js
+++ src/store/store.js
@@ -8,10 +8,10 @@ export default new Vuex.Store({
   state: {
     isNavOpen : false,
     threegui : 2,
-    RESTAPIKEY:'e84e333a2e7f3b24ae892baa9d6102a4', //카카오 App의 rest api key
+    RESTAPIKEY:'47ed5354236c8ddd5cb8b1b7c8e3ca24', //카카오 App의 rest api key
     ADMIN_REDIRECT_URI:'http://localhost:8080/adminKakaoToken', //admin 카카오 토큰 획득 성공시 redirect되는 url
     MEMBER_REDIRECT_URI:'http://localhost:8080/memberKakaoToken', //member 카카오 토큰 획득 성공시 redirect되는 url
-    CLIENT_SECRET:'Umh8oeFqcbkItVe5lQnA0L8pjdgwnrog', //카카오 App의 client secret key
+    CLIENT_SECRET:'7J5S6yn16obNqZ8xrazZ7xhDVk3uUkm3', //카카오 App의 client secret key
     BACK_SERVER:'http://studioj.ddns.net', //HTF 프로젝트의 backend server
     FRONT_SERVER:'http://localhost:8080' //HTF 프로젝트의 frontend server
   },
